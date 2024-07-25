a6b7b67ed3fd7a469fa1038e88185341bad808d5
evamcconville <mcconvilleeva@gmail.com>
2024-07-25 14:08:01 +0100

    did some stuff

--- quote.js
+++ quote.js
@@ -2,31 +2,37 @@ import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
 import { readFile } from 'node:fs/promises';
 
-const fileName = "quotes.json";
+const quotesFile = "quotes.json";
+
+// try to read a file ffs
+
+    
 
 export async function getQuotes() {
-    async function readFromFile(fileName) {
+    
+async function sopranosRead(fileNamePeramiter) {
         try {
-            const data = await readFile(fileName, 'utf8');
-            console.log(data);
+            const fileData = await sopranosRead(quotesFile, 'utf8');
+            console.log(fileData);
         } catch (error) {
             console.error('Error reading file:', error);
         }
     }
-}
-
-export async function addQuote(quoteText) {
 
 }
 
-export async function getRandomQuote() {
+// export async function addQuote(quoteText) {
 
-}
+// }
 
-export async function editQuote(id, quoteText) {
+// export async function getRandomQuote() {
 
-}
+// }
 
-export async function deleteQuote(id) {
+// export async function editQuote(id, quoteText) {
 
-}
+// }
+
+// export async function deleteQuote(id) {
+
+// }
