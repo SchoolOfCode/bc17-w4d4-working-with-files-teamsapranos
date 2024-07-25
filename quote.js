b1461e304fcf468704486d9f3154b9602f57a869
ajbecks <aaron_beckles@hotmail.co.uk>
2024-07-25 13:42:06 +0100

    ticket 2b updated

--- quote.js
+++ quote.js
@@ -1,10 +1,18 @@
 import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
+import { readFile } from 'node:fs/promises';
 
 const fileName = "quotes.json";
 
 export async function getQuotes() {
-    
+    async function readFromFile(fileName) {
+        try {
+            const data = await readFile(fileName, 'utf8');
+            console.log(data);
+        } catch (error) {
+            console.error('Error reading file:', error);
+        }
+    }
 }
 
 export async function addQuote(quoteText) {
